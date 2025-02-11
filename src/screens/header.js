9d1527ad6e3c6f3332838060b9c2edc659b68d68
nuru ibrahim <nuruibrahim11111@gmail.com>
2025-02-11 07:13:08 -0800

    adjust menu applicant history

--- src/screens/header.js
+++ src/screens/header.js
@@ -62,7 +62,7 @@ const Header = () => {
                             <Button color="inherit" onClick={() => navigate('/requestlist')}>Request List</Button>
                             <Button color="inherit" onClick={() => navigate('/signup')}>Add Agent</Button>
                             <Button color="inherit" onClick={() => navigate('/reportlist')}>Report List</Button>
-                            <Button color="inherit" onClick={() => navigate('/applicanthistory')}>History List</Button>
+                            <Button color="inherit" onClick={() => navigate('/applicanthistory')}>Applicant History</Button>
                             <Button color="inherit" onClick={() => navigate('/setting')}>Settings</Button>
                         </>
                     )}
@@ -101,7 +101,7 @@ const Header = () => {
                                 <MenuItem onClick={() => handleNavigation('/requestlist')}>Request List</MenuItem>
                                 <MenuItem onClick={() => handleNavigation('/signup')}>Add User</MenuItem>
                                 <MenuItem onClick={() => handleNavigation('/reportlist')}>Report List</MenuItem>
-                                <MenuItem onClick={() => handleNavigation('/applicanthistory')}>History List</MenuItem>
+                                <MenuItem onClick={() => handleNavigation('/applicanthistory')}>Applicant History</MenuItem>
                                 <MenuItem onClick={() => handleNavigation('/setting')}>Settings</MenuItem>
                                 
                             </>
